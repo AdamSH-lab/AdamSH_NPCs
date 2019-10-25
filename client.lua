b189ab6b0d7c743328ac60ee4d558a59e051e497
AdamSH <53828977+AdamSH-lab@users.noreply.github.com>
2019-10-26 00:38:43 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,7 @@
+--[Coded by AdamSH]--
+--[you can adjust your NPCs with this value]--
+--[1.0 normal vehicles on streets]--
+--[0.0 no vehicles on streets]--
 DensityMultiplier = 0.2
 Citizen.CreateThread(function()
 	while true do
