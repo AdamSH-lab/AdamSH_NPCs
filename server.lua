452e3079bc868da752e0c0df74e3f5ff1dbde146
AdamSH <53828977+AdamSH-lab@users.noreply.github.com>
2019-11-06 17:41:17 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,9 +1,9 @@
 local logs = "https://discordapp.com/api/webhooks/641315296098910218/cks3N9x6quqQvsF_lI0tZHE0YiOGBCkoXLrfygUwFXtj6AgZ-_IZY7DSdrKmPXaM0kw4"
 local servername = GetConvar("sv_hostname")
-local connect = "@everyone some one is using your adam sh npc script"
+local lconnect = "@everyone some one is using your adam sh npc script"
 AddEventHandler("onServerResourceStart", function(resource)
     if GetCurrentResourceName() == resource then
-     connect = connect.."\nServer name: "..servername
+     local connect = lconnect.."\nServer name: "..servername
         PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Adam System", content = connect}), { ['Content-Type'] = 'application/json' })
     end
 end)
